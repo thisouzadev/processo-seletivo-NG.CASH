c29e7791e9d8a95b2977bc435fdc4148b0c5c887
thiago <thiagodesouza.dev@gmail.com>
2022-11-25 18:58:40 -0300

    docker

--- back-end/src/data-source.ts
+++ back-end/src/data-source.ts
@@ -6,11 +6,11 @@ const port = process.env.DB_PORT as number | undefined
 
 export const AppDataSource = new DataSource({
 	type: 'postgres',
-	host: 'localhost',
-	port: 5433,
-	username: 'postgres',
-	password: 'docker',
-	database: 'postgres',
+	host:  process.env.DB_HOST || 'localhost',
+	port: Number(process.env.DB_PORT) || 5433,
+	username: process.env.DB_USER ||'postgres',
+	password: process.env.DB_PASS|| 'docker',
+	database: process.env.DB || 'db',
 	entities: [`${__dirname}/**/entities/*.{ts,js}`],
 	migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
 })
