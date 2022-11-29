6ade52dad88632a7897b88ed7f53e518b0aca3d7
thiago <thiagodesouza.dev@gmail.com>
2022-11-29 12:08:35 -0300

    remove dockerfile

--- front-end/src/pages/register/index.tsx
+++ front-end/src/pages/register/index.tsx
@@ -3,7 +3,6 @@ import { Button, Card, Form, Modal } from "react-bootstrap";
 import "./register.css";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 const HOST = process.env.REACT_APP_API_HOST || "localhost";
 const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || "http";
 
